220c6bef0e7ae9f8841ba9e780c98cc0a8f9075c
Francisco de Asís Cobo Sánchez <fcobsan076@cpifpalanturing.es>
2025-10-03 10:59:21 +0200

    clase cambios

--- app/src/main/java/com/alanturin/proyectolol/data/ChampionRepository.kt
+++ app/src/main/java/com/alanturin/proyectolol/data/ChampionRepository.kt
@@ -9,121 +9,121 @@ object ChampionRepository {
             nombre = "Ahri",
             title = "La Zorra de Nueve Colas",
             lore = "Una vastaya con conexión espiritual a la magia; absorbe la esencia de las almas para conocer recuerdos, mientras lucha entre su naturaleza salvaje y su deseo de humanidad.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg"
         ),
         Champion(
             nombre = "Aatrox",
             title = "La Espada de los Oscuros",
             lore = "Un guerrero antiguo Darkin que busca liberarse de su prisión, despierta en medio de guerras y busca consumir para existir eternamente.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg"
         ),
         Champion(
             nombre = "Akali",
             title = "El Puño de la Sombra",
             lore = "Una asesina de Jonia que forma parte de los Kinkou en un pasado, luego adopta su propio estilo para proteger su tierra y justicia en sus términos.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Akali_0.jpg"
         ),
         Champion(
             nombre = "Garen",
             title = "El Poder de Demacia",
             lore = "Caballero demaciano orgulloso, defensor del reino y opuesto al uso de la magia libre; lucha por el honor y la seguridad de Demacia.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Garen_0.jpg"
         ),
         Champion(
             nombre = "Lux",
             title = "La Dama de la Luminosidad",
             lore = "Hermana de Garen, posee magia en un reino que teme los magos; busca demostrar que la magia puede proteger en lugar de destruir.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Lux_0.jpg"
         ),
         Champion(
             nombre = "Zed",
             title = "El Maestro de las Sombras",
             lore = "Antiguo aprendiz de un orden místico, abrazó técnicas oscuras prohibidas para crear su propia senda de poder y control.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Zed_0.jpg"
         ),
         Champion(
             nombre = "Hecarim",
             title = "La Sombra de la Guerra",
             lore = "Ser espectral mitad hombre mitad bestia, lidera la carga del terror en el reino de la Ruina destruyendo a sus enemigos bajo relinchos de muerte. ",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Hecarim_0.jpg"
         ),
         Champion(
             nombre = "Jinx",
             title = "(Traducción aproximada) La Explosiva",
             lore = "Impulsiva y caótica, Jinx desata el caos en Piltover y Zaun con armas explosivas, impulsada por su pasado oscuro y obsesión por la anarquía.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jinx_0.jpg"
         ),
         Champion(
             nombre = "Teemo",
             title = "El Explorador Veloz",
             lore = "Yordle del cuerpo de exploración de Bandle que recorre Runaterra en misiones, tiene doble faceta: alegre y travieso, pero también peligroso.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Teemo_0.jpg"
         ),
         Champion(
             nombre = "Viego",
             title = "El Rey Arruinado",
             lore = "Rey obsesionado por resucitar a su esposa Isolde; su desesperación desata la corrupción conocida como la Ruina, afectando todo a su paso.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Viego_0.jpg"
         ),
         Champion(
             nombre = "Leona",
             title = "La Aurora Radiante",
             lore = "Leona es una guerrera sagrada del sol que empuña la espada y el escudo para proteger a sus aliados y erradicar las sombras que amenazan Targon.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Leona_0.jpg"
         ),
         Champion(
             nombre = "Darius",
             title = "La Mano de Noxus",
             lore = "Darius es un comandante implacable de Noxus, famoso por su brutalidad y su capacidad para acabar con sus enemigos con una sola mano, inspirando temor y respeto.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Darius_0.jpg"
         ),
         Champion(
             nombre = "Thresh",
             title = "El Guardián de la Corriente Oscura",
             lore = "Thresh es un espectro maldito que captura las almas de los vivos para torturarlas eternamente, moviéndose con la oscuridad y sembrando terror.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Thresh_0.jpg"
         ),
         Champion(
             nombre = "Riven",
             title = "La Exiliada",
             lore = "Riven es una guerrera que busca redención tras romper su espada y exiliarse, intentando encontrar la paz mientras enfrenta su pasado oscuro y violento.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Riven_0.jpg"
         ),
         Champion(
             nombre = "Malphite",
             title = "El Fragmento de Monolito",
             lore = "Malphite es un colosal ser de roca viviente que emerge para proteger la tierra de las amenazas, utilizando una fuerza y resistencia casi impenetrables.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Malphite_0.jpg"
         ),
         Champion(
             nombre = "Sona",
             title = "La Virtuosa de las Cuerdas",
             lore = "Sona es una talentosa musicista que utiliza su arpa mística para sanar, inspirar y proteger a sus aliados, comunicándose sin palabras en medio de la batalla.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Sona_0.jpg"
         ),
         Champion(
             nombre = "Yasuo",
             title = "El Errante",
             lore = "Yasuo es un espadachín marcado por la tragedia que busca limpiar su nombre, utilizando el viento para controlar la batalla y sobrevivir contra todo pronóstico.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yasuo_0.jpg"
         ),
         Champion(
             nombre = "Katarina",
             title = "La Daga Siniestra",
             lore = "Katarina es una asesina implacable de Noxus, conocida por su rapidez, precisión y habilidad para eliminar objetivos con una combinación letal de cuchillas y estrategia.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Katarina_0.jpg"
         ),
         Champion(
             nombre = "Camille",
             title = "La Sombra de Acero",
             lore = "Camille es una guerrera modificada cibernéticamente que usa sus habilidades para proteger Piltover y mantener el orden.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Camille_0.jpg"
         ),
         Champion(
             nombre = "Kha'Zix",
             title = "El Asesino del Vacío",
             lore = "Kha'Zix es una criatura del Vacío que evoluciona al cazar a sus presas, adaptándose para ser un depredador perfecto.",
-            imgUrl = ""
+            imgUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/KhaZix_0.jpg"
         ),
     )
 }
