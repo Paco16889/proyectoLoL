185aee91b3b1fc8aa2283bfd50b913e6f952b0dd
Francisco de Asís Cobo Sánchez <fcobsan076@cpifpalanturing.es>
2025-10-03 10:56:53 +0200

    clase cambios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
 }
 
 dependencies {
-
+// https://mvnrepository.com/artifact/io.coil-kt/coil
+    implementation(libs.coil)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
